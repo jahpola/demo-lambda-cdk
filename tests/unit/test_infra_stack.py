18829f958f8f4aafacdfd7cb6e7d1f242ce16dcd
Jyrki Ahpola <16726281+jahpola@users.noreply.github.com>
2024-10-09 21:42:31 +0300

    Improve tests

--- tests/unit/test_infra_stack.py
+++ tests/unit/test_infra_stack.py
@@ -1,22 +1,25 @@
 """ define tests """
 
-import aws_cdk as core
-
 from cdk_nag import AwsSolutionsChecks
-
 from aws_cdk import (
     assertions,
     Aspects,
+    App,
 )
+
 from infra.infra_stack import InfraStack
+import pytest
 
-
-def test_lambda_function_created():
-    """test that lambda function is created"""
-    app = core.App()
+@pytest.fixture(scope="module",name="template")
+def setup_template():
+    """define a basic app """
+    app = App()
     stack = InfraStack(app, "teststack")
     template = assertions.Template.from_stack(stack)
+    yield template
 
+def test_lambda_function_created(template):
+    """test that lambda function is created"""
     template.has_resource_properties(
         "AWS::Lambda::Function", {"Handler": "main.handler", "Architectures": ["arm64"]}
     )
@@ -24,5 +27,5 @@ def test_lambda_function_created():
 
 def test_solutions_checks():
     """test that solutions checks are in place"""
-    app = core.App()
+    app = App()
     Aspects.of(app).add(AwsSolutionsChecks())
