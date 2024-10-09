53915df10582510d0d93fafa3801f16c258fe6cc
Jyrki Ahpola <16726281+jahpola@users.noreply.github.com>
2024-10-09 20:10:20 +0300

    Initial upgrade

--- infra/infra_stack.py
+++ infra/infra_stack.py
@@ -1,37 +1,36 @@
 from aws_cdk import (
-    Stack,
-    aws_events_targets as targets,
-    aws_events as events,
-    aws_lambda as _lambda,
-    aws_lambda_python_alpha as _lambda_python
+  Stack,
+  aws_events_targets as targets,
+  aws_events as events,
+  aws_lambda as _lambda,
+  aws_lambda_python_alpha as _lambda_python,
 )
 from constructs import Construct
 
 class InfraStack(Stack):
+    ''' create infra '''
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-        
-        my_function = _lambda_python.PythonFunction(self,
+
+        my_function = _lambda_python.PythonFunction(
+            self,
             "my-function",
             entry="src",
             index="main.py",
             handler="handler",
-            runtime=_lambda.Runtime.PYTHON_3_9,
+            architecture=_lambda.Architecture.ARM_64,
+            runtime=_lambda.Runtime.PYTHON_3_12,
         )
 
         rule = events.Rule(
             self,
             "Rule",
             schedule=events.Schedule.cron(
-                minute="0",
-                hour="18",
-                month="*",
-                week_day="MON-FRI",
-                year="*"
-            )
+                minute="0", hour="18", month="*", week_day="MON-FRI", year="*"
+            ),
         )
 
         rule.add_target(targets.LambdaFunction(my_function))
